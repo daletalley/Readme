2f79ac9834775427e1587164ab8786dae44cb81d
Dale Talley III <dtalley32@icloud.com>
2023-10-18 16:12:45 -0500

    fix async

--- Develop/index.js
+++ Develop/index.js
@@ -1,99 +1,54 @@
-// TODO: Include packages needed for this application
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateMarkdown");
+
 const writeFileAsync = util.promisify(fs.writeFile);
 const mkdirAsync = util.promisify(fs.mkdir);
-const accessSync = util.promisify(fs.accessSync);
+const accessAsync = util.promisify(fs.access);
 
 // TODO: Create an array of questions for user input
-const questions = [ 
-  {
-    type: "input",
-    name: "username",
-    message: "Please enter your GitHub username: "
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Please enter your email: "
-  },
-  {
-    type: "input",
-    name: "projectTitle",
-    message: "What is the project title?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Write a brief description of your project: "
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "Describe the installation process if any: ",
-  },
-  {
-      type: "input",
-      name: "dependencies",
-      message: "What command should be run to install dependencies?",
-      default: "npm i",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What is this project usage for?"
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Chose the appropriate license for this project: ",
-    choices: [
-      "Apache",
-      "Academic",
-      "GNU",
-      "ISC",
-      "MIT",
-      "Mozilla",
-      "Open"
-    ]
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "What does the user need to know about contributing to the repo?"
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "What command should be run to run tests?",
-    default: "npm test"
-  },
-  {
-    type: "input",
-    name: "questions",
-    message: "What do I do if I have an issue with the repository?"
-  },
-  {
-    type: "input",
-    name: "username",
-    message: "Please enter your GitHub username: "
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Please enter your email: "
-  }
+const questions = [
+  // ... your questions here
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return writeFileAsync(fileName, data);
-  }
+  return writeFileAsync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  try {
+    // Ask user questions and generate responses
+    const answers = await inquirer.prompt(questions);
+    const generateContent = generateReadme(answers);
+
+    const directoryPath = "./newREADME";
+    const filePath = `${directoryPath}/README.md`;
+
+    // Check if the directory already exists
+    try {
+      await accessAsync(directoryPath, fs.constants.F_OK);
+      console.log("Directory already exists. Skipping creation.");
+    } catch (err) {
+      // Create the directory
+      await mkdirAsync(directoryPath);
+      console.log("Directory created successfully.");
+    }
+
+    // Check if the file already exists
+    try {
+      await accessAsync(filePath, fs.constants.F_OK);
+      console.log("File already exists. Skipping creation.");
+    } catch (err) {
+      // Write new README.md to the directory
+      await writeToFile(filePath, generateContent);
+      console.log("✨  Successfully created a beautiful README.md in the 'newREADME' directory! ✨");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // Function call to initialize app
 init();
