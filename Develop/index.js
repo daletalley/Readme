6ef512881abf874e6c669ed193a611c13268bced
Dale Talley III <dtalley32@icloud.com>
2023-10-18 15:46:37 -0500

    Include packages needed for this application

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,11 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
+const mkdirAsync = util.promisify(fs.mkdir);
+const accessSync = util.promisify(fs.accessSync);
 // TODO: Create an array of questions for user input
 const questions = [];
 
